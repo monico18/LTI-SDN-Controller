84ba044738163f36500b6292246d6bd9d924199e
tomas <2201762@my.ipleiria.pt>
2024-04-02 17:44:23 +0100

    Add node functionality and table refresh

--- sdn-controller.py
+++ sdn-controller.py
@@ -3,21 +3,62 @@ from ui_main import Ui_MainWindow
 from login import Ui_LoginPage
 import sys
 import requests
-from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData
+import atexit
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, delete
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
-    def __init__(self):
+    def __init__(self, nodes):
         super(MainWindow, self).__init__()
         self.setupUi(self)
+
+        self.nodes = nodes
+
 
         self.btn_page_1.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.page_1))
         self.btn_page_2.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.page_2))
         self.btn_page_3.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.page_3))
 
+        self.btn_add.clicked.connect(self.add_node)
 
-        self.btn_add.clicked.connect()
+    def add_node(self):
+        username = self.lineEdit.text()
+        password = self.lineEdit_2.text()
+        ip_address = self.lineEdit_3.text()
+        name = self.lineEdit_4.text()
 
+        ins = self.nodes.insert().values(username=username, password=password, ip_address=ip_address, name=name)
+        engine.execute(ins)
+
+        print("Node added successfully!")
+
+        self.lineEdit.clear()
+        self.lineEdit_2.clear()
+        self.lineEdit_3.clear()
+        self.lineEdit_4.clear()
+
+        self.refresh_table()
+        pass
+
+    def refresh_table(self):
+        # Clear the table
+        self.routerTable.setRowCount(0)
+
+        # Add items to the table
+        for node in self.get_nodes():
+            row = self.routerTable.rowCount()
+            self.routerTable.insertRow(row)
+            self.routerTable.setItem(row, 0, QtWidgets.QTableWidgetItem(node['name']))
+            self.routerTable.setItem(row, 1, QtWidgets.QTableWidgetItem(node['ip_address']))
+        pass
+
+    def get_nodes(self):
+        global nodes
+        s = self.nodes.select().where(self.nodes.c.id > 0)
+        result = engine.execute(s)
+        nodes = result.fetchall()
+        return nodes
+    
 class LoginPage(QtWidgets.QMainWindow, Ui_LoginPage):
     def __init__(self):
         super(LoginPage, self).__init__()
@@ -42,13 +83,19 @@ class LoginPage(QtWidgets.QMainWindow, Ui_LoginPage):
             print("Login successful!")
             # Change to a specific page in the stacked widget
             self.hide()
-            self.main_window = MainWindow()
+            self.main_window = MainWindow(nodes)
             self.main_window.showMaximized()
             self.stacked_widget.setCurrentIndex(1)
         else:
             QtWidgets.QMessageBox.critical(self, "Error", "Invalid username or password.")
             print("Invalid username or password.")
 
+def clear_db():
+    # Create a delete statement that deletes all rows from the nodes table
+    del_stmt = delete(nodes)
+
+    # Execute the delete statement
+    engine.execute(del_stmt)
 
 if __name__ == "__main__":
 
@@ -56,8 +103,8 @@ if __name__ == "__main__":
 
     metadata = MetaData()
 
-    users = Table('Routers', metadata,
-    Column('id', Integer, primary_key=True),
+    nodes = Table('Nodes', metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
     Column('username', String),
     Column('password', String),
     Column('ip_address', String),
@@ -67,12 +114,12 @@ if __name__ == "__main__":
     metadata.create_all(engine)
 
     app = QtWidgets.QApplication(sys.argv)
-    
+    clear_db()
     login_page = LoginPage()
-    main_window = MainWindow()
+    main_window = MainWindow(nodes)
 
     login_page.set_stacked_widget(main_window.stackedWidget)
 
     login_page.show()
-
+    atexit.register(clear_db)
     sys.exit(app.exec_())
