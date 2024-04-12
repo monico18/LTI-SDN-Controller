bce8e97781329aea9701838045a164fd8a7ffc35
Tomas <2201762@my.ipleiria.pt>
2024-04-13 00:04:30 +0100

    Update DHCP server API calls to use HTTPBasicAuth for authentication

--- dhcp_queries.py
+++ dhcp_queries.py
@@ -1,9 +1,10 @@
 import requests
+from requests.auth import HTTPBasicAuth
 
 def add_dhcp_server(username, password, host, dhcp_server_config):
     try:
         url = f"https://{host}/rest/ip/dhcp-server/add"
-        response = requests.post(url, auth=(username, password), data=dhcp_server_config, verify=False)
+        response = requests.post(url, auth=HTTPBasicAuth(username, password), data=dhcp_server_config, verify=False)
         return response.json()
 
     except Exception as e:
@@ -12,7 +13,7 @@ def add_dhcp_server(username, password, host, dhcp_server_config):
 def delete_dhcp_server(username, password, host, dhcp_server_id):
     try:
         url = f"http://{host}/rest/ip/dhcp-server/{dhcp_server_id}"
-        response = requests.delete(url, auth=(username, password), data={'.id': dhcp_server_id}, verify=False)
+        response = requests.delete(url, auth=HTTPBasicAuth(username, password), data={'.id': dhcp_server_id}, verify=False)
         return response.json()
 
     except Exception as e:
@@ -21,7 +22,7 @@ def delete_dhcp_server(username, password, host, dhcp_server_id):
 def edit_dhcp_server(username, password, host, dhcp_server_id, dhcp_server_config):
     try:
         url = f"https://{host}/rest/ip/dhcp-server/{dhcp_server_id}"
-        response = requests.patch(url, auth=(username, password), data={'.id': dhcp_server_id, **dhcp_server_config}, verify=False)
+        response = requests.patch(url, auth=HTTPBasicAuth(username, password), data={'.id': dhcp_server_id, **dhcp_server_config}, verify=False)
         return response.json()
 
     except Exception as e:
@@ -30,7 +31,7 @@ def edit_dhcp_server(username, password, host, dhcp_server_id, dhcp_server_confi
 def get_available_dhcp_servers(username, password, host):
     try:
         url = f"https://{host}/rest/ip/dhcp-server"
-        response = requests.get(url, auth=(username, password), verify=False)
+        response = requests.get(url, auth=HTTPBasicAuth(username, password), verify=False)
         dhcp_servers = response.json().get('data', [])
         return dhcp_servers
 
